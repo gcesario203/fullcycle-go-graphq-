1b721904a8e0a977b4caf4817573c8c6af367a1c
Gabriel Cesario <gcesario203@gmail.com>
2023-01-12 11:10:43 +0000

    feat: Resolver de criação de categoria

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,13 @@
 package graph
 
+import (
+	database "github.com/gcesario203/fullcycle-go-graphq-/internal/db"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
+	CategoryDB *database.Category
 }
